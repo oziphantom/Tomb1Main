c98c9552853b2a97452749cdbe5727047371c6b3
oziphantom <oziphantom@users.noreply.github.com>
2021-10-12 01:30:41 +1100

    Fixed incorrect string copy in args
    it was missing the +1, so you could end up with garbage at the end of the string causing either a crash at some point if you where unlucky or mostly just -gold to fail due to garbage in memory.

--- src/args.c
+++ src/args.c
@@ -20,9 +20,9 @@ int get_command_line(char ***args, int *arg_count)
     *args = malloc(l_arg_count * sizeof(char **));
     *arg_count = l_arg_count;
     for (int i = 0; i < l_arg_count; i++) {
-        size_t size = wcslen(l_arg_list[i]);
-        (*args)[i] = malloc(size);
-        wcstombs((*args)[i], l_arg_list[i], size);
+        size_t size = wcslen(l_arg_list[i]);          //this is the size without the NULL terminator
+        (*args)[i] = malloc(size+1);                  //alloc one more to hold the null
+        wcstombs((*args)[i], l_arg_list[i], size+1);  //allow it to also look at the NULL so the string is terminated
     }
 
     LocalFree(l_arg_list);
